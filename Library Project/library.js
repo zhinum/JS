fea628b374ec7a42ecfeea76a6ffd26a3829144d
zhinum <princeonah23@gmail.com>
2026-03-25 08:49:02 +0100

    Refactored using classes

--- Library Project/library.js
+++ Library Project/library.js
@@ -1,131 +1,143 @@
-const formButton = document.querySelector(".form-button");
-const dialog = document.querySelector("dialog");
-const form = document.querySelector(".form");
-const submitButton = document.querySelector(".submit-button");
-const author = document.querySelector("#author");
-const title = document.querySelector("#title");
-const pages = document.querySelector("#pages");
-const read = document.querySelector("#read");
-const tableBody = document.querySelector("tbody");
+class Book {
+  static #library = [];
+  constructor(title, author, pages, ifRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.ifRead = ifRead;
 
-let library = [];
-
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("you must input new operator");
+    Book.#library.push(this);
   }
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.checkIfRead = function () {
-    if (this.read === true) {
-      return `${this.title} has been read`;
-    } else {
-      return `${this.title} has not been read`;
+  static get allbooks() {
+    return Book.#library;
+  }
+  static updateLibrary(newList) {
+    Book.#library = newList;
+  }
+  get readStatus() {
+    return this.ifRead;
+  }
+}
+
+class Display {
+  static #gridContainer = document.querySelector(".Grid-container");
+
+  static clear() {
+    Display.#gridContainer.innerHTML = "";
+  }
+  static get grid() {
+    return this.#gridContainer;
+  }
+
+  static render() {
+    const books = Book.allbooks;
+    Display.clear();
+    books.forEach(({ id, title, author, pages, ifRead }) => {
+      const card = document.createElement("div");
+      card.classList.add("book-card");
+      card.dataset.id = id;
+      card.innerHTML = `
+      <h3 class="book-title"></h3>
+      <p class="book-id"></p>
+      <p class="book-Author"></p>
+      <p><span class="book-pages"></span></p>
+      <p class="ifRead"></p>
+      <div class="card-buttons">
+       <button class="delete-button">Delete</button>
+       <button class="toggle-button">Status</button>
+      </div>
+    `;
+      card.querySelector(".book-title").textContent = title;
+      card.querySelector(".book-id").textContent = id;
+      card.querySelector(".book-Author").textContent = author;
+      card.querySelector(".book-pages").textContent = pages;
+      card.querySelector(".ifRead").textContent = ifRead ? "Read" : "unread";
+
+      Display.#gridContainer.appendChild(card);
+    });
+  }
+}
+class mainControl {
+  static subControl() {
+    Display.grid.addEventListener("click", (e) => {
+      const card = e.target.closest(".book-card");
+      if (!card) return;
+
+      const id = card.dataset.id;
+      if (e.target.classList.contains("delete-button")) {
+        mainControl.removeBook(id);
+      } else if (e.target.classList.contains("toggle-button")) {
+        mainControl.toogleRead(id);
+      }
+    });
+  }
+
+  static removeBook(id) {
+    const filteredList = Book.allbooks.filter((book) => book.id !== id);
+    Book.updateLibrary(filteredList);
+    Display.render();
+  }
+  static toogleRead(id) {
+    const book = Book.allbooks.find((b) => b.id === id);
+
+    if (book) {
+      book.ifRead = !book.ifRead;
+      Display.render();
     }
-  };
-}
-function storeBook(newBook) {
-  library.push(newBook);
-  return library;
-}
-function renderBooks() {
-  for (const book of library) {
-    const tableRow = document.createElement("tr");
-    const tableId = document.createElement("td");
-    const tableTitle = document.createElement("td");
-    const tableAuthor = document.createElement("td");
-    const tablePages = document.createElement("td");
-    const tableRead = document.createElement("td");
-    const tableAction = document.createElement("td");
-    const deleteButton = document.createElement("button");
-    const toogleButtton = document.createElement("button");
-    toogleButtton.classList.add("btn-toogle");
-    toogleButtton.setAttribute("style", "padding:8px");
-    tableId.textContent = book.id;
-    tableTitle.textContent = book.title;
-    tableAuthor.textContent = book.author;
-    tablePages.textContent = book.pages;
-    tableRead.textContent = book.read ? "Yes" : "No";
-    deleteButton.textContent = "Delete";
-    deleteButton.classList.add("btn-delete");
-    tableRow.appendChild(tableId);
-    tableRow.appendChild(tableTitle);
-    tableRow.appendChild(tableAuthor);
-    tableRow.appendChild(tablePages);
-    tableRow.appendChild(tableRead);
-    tableRead.appendChild(toogleButtton);
-    tableAction.appendChild(deleteButton);
-    tableRow.appendChild(tableAction);
-
-    tableBody.appendChild(tableRow);
-    tableRow.dataset.id = book.id;
   }
 }
-function deleteBook(bookid) {
-  library = library.filter((book) => book.id !== bookid);
-  return library;
-}
-function toogleRead(bookid) {
-  const book = library.find((book) => book.id === bookid);
-  book.read = !book.read;
-  return book;
-}
-function displayForm() {
-  dialog.show();
-}
-function createFromForm() {
-  const newFormBook = new Book(
-    title.value,
-    author.value,
-    +pages.value,
-    read.checked,
-  );
-  return newFormBook;
-}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const newBook = createFromForm();
-  storeBook(newBook);
-  clearTable();
-  renderBooks();
-});
-function clearTable() {
-  tableBody.innerHTML = "";
-}
-formButton.addEventListener("click", () => {
-  displayForm();
-});
-submitButton.addEventListener("click", () => {
-  dialog.close();
-});
-tableBody.addEventListener("click", (e) => {
-  const row = e.target.closest("tr");
-  const bookID = row.dataset.id;
-  if (e.target.closest(".btn-delete")) {
-    deleteBook(bookID);
-  }
-  if (e.target.closest(".btn-toogle")) {
-    toogleRead(bookID);
+class FormDialog {
+  static #form = document.querySelector("#book-form");
+  static #dialog = document.querySelector("#book-dialog");
+  static formControl() {
+    FormDialog.#form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      FormDialog.submit();
+    });
   }
 
-  clearTable();
-  renderBooks();
-});
+  static open() {
+    const openButton = document.querySelector(".open-button");
+    openButton.addEventListener("click", () => {
+      FormDialog.#dialog.showModal();
+    });
+  }
+  static close() {
+    FormDialog.#dialog.close();
+    FormDialog.#form.reset();
+  }
+
+  static submit() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const ifRead = document.querySelector("#ifRead").checked;
+    new Book(title, author, pages, ifRead);
+
+    Display.render();
+    FormDialog.close();
+  }
+  static formButtonControl() {
+    const formButtons = document.querySelector(".form-buttons");
+    formButtons.addEventListener("click", (e) => {
+      if (e.target.classList.contains("cancel-btn")) {
+        FormDialog.close();
+      }
+    });
+  }
+}
+
 // practice books
 const book1 = new Book("Return", "Prince Onah", 469, true);
-storeBook(book1);
-book1.checkIfRead();
+
 const book2 = new Book("Imagine", "Prince Onah", 678, false);
-storeBook(book2);
-book2.checkIfRead();
+
 const book3 = new Book("Dream", "Prince Onah", 57, false);
-storeBook(book3);
-book3.checkIfRead();
+
 const book4 = new Book("Manage", "Prince Onah", 78, true);
-storeBook(book4);
-book4.checkIfRead();
-renderBooks();
+Display.render();
+mainControl.subControl();
+FormDialog.open();
+FormDialog.formButtonControl();
+FormDialog.formControl();
